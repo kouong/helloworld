1248b39fc4886c5e7be0e21061dbe4d68c82fde3
Gerard Kouonfo <Gerard.KouonfoNjiti@emeal.nttdata.com>
2023-06-16 01:00:32 +0200

    fix work

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'It finally worked!!';
+    return 'It finally worked!!!!!!!!';
   }
 }
