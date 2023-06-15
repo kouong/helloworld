ceb6d80c609d536d20cf1f88b6f3427483849de1
Gerard Kouonfo <Gerard.KouonfoNjiti@emeal.nttdata.com>
2023-06-15 22:53:30 +0200

    changed text

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Goodbye world"', () => {
+      expect(appController.getHello()).toBe('Goodbye world');
     });
   });
 });
